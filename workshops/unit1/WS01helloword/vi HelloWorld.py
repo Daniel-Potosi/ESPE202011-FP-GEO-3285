baca87d29409e6d78fd578ed49022b666b44b16c
dapotosi <dani_potosi_00@hotmail.com>
2021-01-08 13:42:03 -0500

    worshops

---
+++ workshops/unit1/WS01helloword/vi HelloWorld.py
@@ -0,0 +1,11 @@
+print("Hello World from Daniel Potosi \n ESPE \n GEO")
+
+# input variables
+addend1 = input('Enter the first number')
+addend2 = input('Enter the second number')
+
+# add two numbers 
+sum = float(addend2) + float(addend1)
+
+# displaying the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
