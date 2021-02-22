b002e3b9fbf8e672885b9aad97a5ae13ecb50efa
dapotosi <dani_potosi_00@hotmail.com>
2021-02-21 21:30:46 -0500

    workshops

--- workshops/unit2/WS14(Arrays)/src/ws14/arrays/WS14Arrays.java
+++ workshops/unit2/WS14(Arrays)/src/ws14/arrays/WS14Arrays.java
@@ -1,91 +1,43 @@
 package ws14.arrays;
 
-import java.util.Scanner;
-
 public class WS14Arrays {
 
-    /**
-     *
-     * @param args
-     */
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] bikes;
+        String[] bikes = {"mountain bike", "rute", "mtb"};
+        float[] bikes = { 1F, 2F, 4F, 8F, 3F, 5F};
         Scanner input = new Scanner(System.in);
-        String leave = "";
-        while (!leave.equalsIgnoreCase("===Leave====")) {
-            System.out.println(" ====Hello====");
-            System.out.println(" ====My mane is Daniel====");
-            System.out.println(" _____________ ");
-            System.out.println(" _____________ ");
-            System.out.println(" ======== OPTION MENU ========= ");
-            System.out.println("1. ====Displacement Formula====");
-            System.out.println("2. ====Final speed====");
-            System.out.println("3. ====Harmonic Movement====");
-            System.out.println(" ======== 4. Exit====");
-            System.out.println(" _____________ ");
-            System.out.println(" _____________ ");
-            System.out.println(" ====Enter an option====");
-            leave = input.nextLine();
+        int n;
+        System.out.println("Enter the types of bikes -> ");
+        n = input.nextInt();
 
-            if (leave.equals("1")) {
-                System.out.println("Displacement Formula");
-                printDisplacement(input);
+        bikes = new int[n];
+        //cbikes = new String[20];
+        //numberofbicyclesperwarehouse = new float[n];
 
-            } else if (leave.equals("2")) {
-                System.out.println("Formula Final speed");
-                printFinalSpeed(input);
-            } else if (leave.equals("3")) {
-                System.out.println("Formula Armonico");
-                printArmonico(input);
-            }
-            if (leave.equals("4")) {
-                System.out.println("=====BYE USER====");
-                System.out.println("====THANKS FOR CHOOSING US===");
-                System.exit(0);
-
-            }
+//        bikes[0] = 7;
+//        bikes[1] = 9;
+//        bikes[2] = 3;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the number of bikes of the warehouse " + (i + 1) + " -> ");
+            bikes[i] = input.nextInt();
         }
-    }
 
-    private static void printArmonico(Scanner input) {
-        System.out.println("Formula: T = 2pi * square root of 1 / g");
-        System.out.println("====Enter to two pi====");
-        double twoPi = input.nextDouble();
-        System.out.println("====Enter to gsquare root of 1 / g====");
-        double squarerootof1 = input.nextDouble();
-        double outcome = ((twoPi * squarerootof1));
-        System.out.println("====ANSWER====");
-        System.out.println("Final speed = " + twoPi + "*" + squarerootof1);
-        System.out.println("Final speed = " + outcome);
-        System.out.println("=============");
-    }
+        for (int bikes : bikesOfTheWarehouse) {
+            System.out.println("bikes of warehouse are " + bikes);
+        }
 
-    private static void printFinalSpeed(Scanner input) {
-        System.out.println("Formula: Final speed = initial velocity - gravity*time");
-        System.out.println("====Enter to initial velocity====");
-        double initialVelocity = input.nextDouble();
-        System.out.println("====Enter to gravity====");
-        double gravity = input.nextDouble();
-        System.out.println("====Enter to time====");
-        double time = input.nextDouble();
-        double outcome = ((initialVelocity - gravity) * (time));
-        System.out.println("====ANSWER====");
-        System.out.println("Final speed = " + initialVelocity + "-" + gravity + "*" + time);
-        System.out.println("Final speed = " + outcome);
-        System.out.println("=============");
-    }
-    private static void printDisplacement(Scanner input) {
-        System.out.println("Formula: x = initial velocity*time + 1/2 acceleration * time^2 ");
-        System.out.println("====Enter to initial velocity====");
-        double initialVelocity = input.nextDouble();
-        System.out.println("====Enter to time====");
-        double time = input.nextDouble();
-        System.out.println("====Enter to acceleration====");
-        double acceleration = input.nextDouble();
-        double outcome = ((initialVelocity * time) + (acceleration / 2) * (time * time));
-        System.out.println("====ANSWER====");
-        System.out.println("x = " + initialVelocity + "*" + time + " + 1/2 *" + acceleration + " * " + time + "^2");
-        System.out.println("x = " + outcome);
-        System.out.println("===============");
+        for (int i = 0; i < n; i++) {
+            System.out.println("bikes of the warehouse " + (i + 1) + " -> " + bikes[i]);
+        }
+
+        for (String bike : bikes) {
+            System.out.println("type bike -> " + bike);
+        }
+        
+        for (int i = 0; i < weightsByWarehouse.length ; i++) {
+            System.out.println("weight of warehouse " + (i + 1) + " -> " + weightsWarehouse[i]);
     }
     
 }
