81a5ff1b06193ea96a9b523eb0e8993e52555245
Daniel Alejandro Potosí Barahona <75495605+dapotosi@users.noreply.github.com>
2021-03-21 22:27:20 -0500

    Add files via upload

---
+++ assignments/UNIT 3/HW18(Arrange)/src/ArrangePage.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andres Ruiz
+ */
+public class ArrangePage {
+     public int arrange(int[] elements, int x){
+        int n = elements.length;
+        for(int i = 0; i < n; i++){
+        }
+        int i = 0;
+        if(elements[i] == x)
+            return i;
+        return n;
+    }
+    
+}
---
+++ assignments/UNIT 3/HW18(Arrange)/src/hw18/arrange/HW18Arrange.java
@@ -0,0 +1,29 @@
+package hw18.arrange;
+
+import java.util.Scanner;
+
+public class HW18Arrange {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        int i = 0;
+        int n = 0;
+        int x;
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+        int e = input.nextInt();
+        int f = input.nextInt();
+        int g = input.nextInt();
+        
+        System.out.println(" Arrange of data ");
+        System.out.println(" Enter the data to order");
+        int[] elements = {a, b, c, d, e, f, g};
+        
+        System.out.println(" The ordered data is as follows ");
+            System.out.print(elements[i] + "  ");
+        }
+    }
